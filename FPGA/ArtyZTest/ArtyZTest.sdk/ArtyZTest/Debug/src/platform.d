8efa49417378e72373bd9d2e1425e4797b3368d9
mitchell orsucci <morsucci@uw.edu>
2017-06-29 11:57:20 -0700

    Deleted redundant directory and added ArtyZfirst project for Vivado

---
+++ FPGA/ArtyZTest/ArtyZTest.sdk/ArtyZTest/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../ArtyZTest_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../ArtyZTest_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../ArtyZTest_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../ArtyZTest_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../ArtyZTest_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../ArtyZTest_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../ArtyZTest_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../ArtyZTest_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
